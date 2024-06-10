7e24d7cc01bdb327b824798cc7c0491480699f16
charlesbiveu <cbiveu@gmail.com>
2024-06-10 18:48:08 -0300

    exercio 07 `M2S04-Ex07.sql` file

--- todosExercicios.sql
+++ todosExercicios.sql
@@ -269,3 +269,32 @@ FROM pedidos p
 JOIN clientes c ON p.cliente_id = c.cliente_id
 ORDER BY p.valor DESC
 LIMIT 1;
+
+
+-- [M2S04] - Ex. 07 - Consultas com Funções Agregadas e Subconsultas
+-- Criar um arquivo SQL com os seguintes procedimentos:
+-- Função Agregada: Encontre a média de idade dos clientes que têm pedidos com valor total superior a 300,00.
+-- Subconsulta: Liste todos os produtos que foram pedidos mais de uma vez.
+
+
+-- [M2S04] - Ex. 07 - Consultas com Funções Agregadas e Subconsultas
+
+-- 1. Função Agregada: Encontre a média de idade dos clientes que têm pedidos com valor total superior a 300,00
+SELECT AVG(c.idade) AS media_idade
+FROM clientes c
+WHERE c.cliente_id IN (
+    SELECT p.cliente_id
+    FROM pedidos p
+    GROUP BY p.cliente_id
+    HAVING SUM(p.valor) > 300.00
+);
+
+-- 2. Subconsulta: Liste todos os produtos que foram pedidos mais de uma vez
+SELECT pr.produto_id, pr.nome_produto, pr.preco
+FROM produtos pr
+WHERE pr.produto_id IN (
+    SELECT ip.produto_id
+    FROM itens_pedidos ip
+    GROUP BY ip.produto_id
+    HAVING COUNT(ip.produto_id) > 1
+);
